d57769b1cfc645454b59165942fade7399fdab7d
Satyam Srivastava <104303280+satya7041@users.noreply.github.com>
2024-07-23 13:37:05 +0530

    Update EmailSection.jsx
    Escape single quotes: Replace single quotes (') with their corresponding HTML entities where they appear in your JSX code. Example: It's should be written as It&apos;s

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -142,7 +142,9 @@ const EmailSection = ()=>{
             <p 
             className="text-[#ADB7BE] mb-4 max-w-md">
                 {" "}
-                I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+                I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, 
+{/*                 to write I'll use I&apos;ll */}
+                I&apos;ll try my best to get back to you!
              </p>
              <div className="socials flex flex-row gap-2">
                 <Link href="https://github.com/satya7041">
