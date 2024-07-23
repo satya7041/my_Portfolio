b85c4d68bb711fa923c8293a7de75320e3f244a1
Satyam Srivastava <104303280+satya7041@users.noreply.github.com>
2024-07-23 13:45:58 +0530

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -138,7 +138,7 @@ const EmailSection = ()=>{
         <div className="z-10">
             <h5 
             className="text-xl font-bold text-white my-2"
-            >Let's Connect</h5>
+            >Let&apos;s Connect</h5>
             <p 
             className="text-[#ADB7BE] mb-4 max-w-md">
                 {" "}
